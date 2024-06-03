6c1bc7af1b8c399faee312943d316923f6274100
abrilcano <147972578+abrilcano@users.noreply.github.com>
2024-06-03 05:54:10 -0500

    Finally v1

---
+++ job.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+#SBATCH --job-name=AbrilGiorgio
+#SBATCH --time=00:10:00
+#SBATCH --nodes=1
+#SBATCH --ntasks=2
+#SBATCH --output=job.out
+#SBATCH --error=stdout.txt
+
+module load singularity
+export TMPDIR=$HOME/tmp
+mkdir -p $TMPDIR
+
+singularity exec --bind $TMPDIR:$TMPDIR container.sif bash -c "export OMPI_MCA_tmpdir_base=$TMPDIR && mpirun -np 2 /prg/main"
+
+
